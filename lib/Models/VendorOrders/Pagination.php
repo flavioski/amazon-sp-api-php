ee83fe6d72bbf7205fbd8ea8523cb63ec7fcda96
flaviopellizzer <flappio.pelliccia@gmail.com>
2021-09-07 16:30:56 +0200

    Vendor Orders Api /Models/VendorOrders/Pagination.php :: generated from swagger 2.0
    source
    https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/vendor-orders-api-model/vendorOrders.json
    
    converted to YAML
    https://editor.swagger.io/#/
    
    using swagger-codegen tools
    https://github.com/swagger-api/swagger-codegen

--- lib/Models/VendorOrders/Pagination.php
+++ lib/Models/VendorOrders/Pagination.php
@@ -5,7 +5,7 @@
  * PHP version 5
  *
  * @category Class
- * @package  Swagger\Client
+ * @package  ClouSale\AmazonSellingPartnerAPI
  * @author   Swagger Codegen team
  * @link     https://github.com/swagger-api/swagger-codegen
  */
@@ -27,16 +27,16 @@
  * Do not edit the class manually.
  */
 
-namespace Swagger\Client\Model;
+namespace ClouSale\AmazonSellingPartnerAPI\Models\VendorOrders;
 
 use \ArrayAccess;
-use \Swagger\Client\ObjectSerializer;
+use \ClouSale\AmazonSellingPartnerAPI\ObjectSerializer;
 
 /**
  * Pagination Class Doc Comment
  *
  * @category Class
- * @package  Swagger\Client
+ * @package  ClouSale\AmazonSellingPartnerAPI
  * @author   Swagger Codegen team
  * @link     https://github.com/swagger-api/swagger-codegen
  */
